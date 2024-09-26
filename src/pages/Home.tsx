a86b83fa1f0e04564fcbd1536293a650151db5b8
Gabriellopessdev <gabriellopessdev@gmail.com>
2024-09-26 14:09:03 -0300

    Estilização da page Success

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,59 +5,61 @@ import CoffeeImageBg from '../assets/images/hero-bg.svg'
 
 export function Home() {
     return (
-        <main className="flex justify-center">
-            <div className="w-1/2 p-8">
-                <div >
-                    <h1 className="text-6xl">Encontre o café perfeito para qualquer hora do dia</h1>
-                    <p className="text-2xl mt-4">Com o Coffe Delivery você recebe seu café onde estiver, a qualquer hora</p>
-                </div>
-                <div className="flex mt-16">
-                    <div>
-                        <div className="mr-4 flex">
-                            <ShoppingCart
-                                size={32}
-                                weight="fill"
-                                className='bg-yellow-600 text-white p-2 rounded-full'
-                            />
-                            <span>Compra simples e segura</span>
+        <div>
+            <main className="flex justify-center mt-12">
+                <div className="w-1/2 p-8">
+                    <div >
+                        <h1 className="text-6xl">Encontre o café perfeito para qualquer hora do dia</h1>
+                        <p className="text-2xl mt-4">Com o Coffe Delivery você recebe seu café onde estiver, a qualquer hora</p>
+                    </div>
+                    <div className="flex mt-16">
+                        <div>
+                            <div className="flex items-center mr-4">
+                                <ShoppingCart
+                                    size={32}
+                                    weight="fill"
+                                    className='bg-yellow-600 text-white p-2 rounded-full'
+                                />
+                                <span className="ml-2">Compra simples e segura</span>
+                            </div>
+                            <div className="flex items-center mr-4 mt-6">
+                                <Timer
+                                    size={32}
+                                    weight="fill"
+                                    className='bg-yellow-400 text-white p-2 rounded-full'
+                                />
+                                <span className="ml-2">Entrega rápida e rastreada</span>
+                            </div>
                         </div>
-                        <div className="flex mr-4 mt-6">
-                            <Timer
-                                size={32}
-                                weight="fill"
-                                className='bg-yellow-400 text-white p-2 rounded-full'
-                            />
-                            <span>Entrega rápida e rastreada</span>
+                        <div>
+                            <div className="flex items-center ml-4">
+                                <Package
+                                    size={32}
+                                    weight="fill"
+                                    className='bg-gray-700 text-white p-2 rounded-full'
+                                />
+                                <span className="ml-2">Embalagem mantém o café intacto</span>
+                            </div>
+                            <div className="flex items-center ml-4 mt-6">
+                                <Coffee
+                                    size={32}
+                                    weight="fill"
+                                    className='bg-purple-600 text-white p-2 rounded-full'
+                                />
+                                <span className="ml-2">O café chega fresquinho até você</span>
+                            </div>
                         </div>
                     </div>
-                    <div>
-                        <div className="ml-4 flex">
-                            <Package
-                                size={32}
-                                weight="fill"
-                                className='bg-gray-700 text-white p-2 rounded-full'
-                            />
-                            <span>Embalagem mantém o café intacto</span>
-                        </div>
-                        <div className="flex ml-4 mt-6">
-                            <Coffee
-                                size={32}
-                                weight="fill"
-                                className='bg-purple-600 text-white p-2 rounded-full'
-                            />
-                            <span>O café chega fresquinho até você</span>
-                        </div>
-                    </div>
                 </div>
-            </div>
-            <div className="flex relative w-1/2 mr-8">
-                <img
-                    src={CoffeeImageBg}
-                    className="absolute inset-0 w-full h-full object-cover"
-                />
-                <img src={CoffeeImage} className="relative z10 mx-auto" />
-            </div>
-
-        </main>
+                <div className="flex relative w-1/2 mr-8 mt-10">
+                    <img
+                        src={CoffeeImageBg}
+                        className="absolute inset-0 w-full h-full object-cover"
+                    />
+                    <img src={CoffeeImage} className="relative z-10 mx-auto" />
+                </div>
+            </main>
+            <Menu />
+        </div>
     )
 }
--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,5 +1,58 @@
+import { CurrencyDollar, MapPin, Timer } from "phosphor-react";
+import delivery from '../assets/images/delivery.svg'
+
 export function Success() {
     return (
-        <h1>Pedido realizado com sucesso!</h1>
+
+        <main className="flex justify-center">
+            <div className="w-1/2 p-8">
+                <div >
+                    <h1 className="text-6xl text-yellow-600">Uhu! Pedido confirmado</h1>
+                    <p className="text-2xl mt-4">Agora é só esperar qure logo o café chegará até você</p>
+                </div>
+                <div className="mt-16 relative w-full p-[2px] rounded-[6px_36px_6px_36px] bg-gradient-to-br from-yellow-500 to-purple-500">
+                    <div className="p-10 items-center w-full h-full bg-white rounded-[6px_36px_6px_36px]">
+                        <div className="flex items-center">
+                            <MapPin
+                                size={32}
+                                weight="fill"
+                                className='bg-purple-600 text-white p-2 rounded-full'
+                            />
+                            <div className="ml-2 flex flex-col">
+                                <span className="ml-2">Entrega em <strong>Rua1</strong></span>
+                                <p className="ml-2"><strong>Japaraiba</strong></p>
+                            </div>
+                        </div>
+                        <div className="flex items-center mt-6">
+                            <Timer
+                                size={32}
+                                weight="fill"
+                                className='bg-yellow-400 text-white p-2 rounded-full'
+                            />
+                            <div className="ml-2 flex flex-col">
+                                <span className="ml-2">Previsão de entrega</span>
+                                <p className="ml-2"><strong>20min - 30min</strong></p>
+                            </div>
+                        </div>
+                        <div className="flex items-center mt-6">
+                            <CurrencyDollar
+                                size={32}
+                                weight="fill"
+                                className='bg-yellow-600 text-white p-2 rounded-full'
+                            />
+                            <div className="ml-2 flex flex-col">
+                                <span className="ml-2">Pagamento na entrega</span>
+                                <p className="ml-2"><strong>Dinheiro</strong></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex w-1/2 ">
+                <img
+                    src={delivery}
+                />
+            </div>
+        </main>
     )
 }
